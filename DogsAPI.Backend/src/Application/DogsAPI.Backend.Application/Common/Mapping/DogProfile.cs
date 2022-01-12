87769f781d1d1486cc907f9eada63aeae5a521df
anechyt <anechytailknu@gmail.com>
2022-01-12 23:14:18 +0200

    Created Dog Dto and mapping

---
+++ DogsAPI.Backend/src/Application/DogsAPI.Backend.Application/Common/Mapping/DogProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using DogsAPI.Backend.Core.Dtos;
+using DogsAPI.Backend.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogsAPI.Backend.Application.Common.Mapping
+{
+    public class DogProfile : Profile
+    {
+        public DogProfile()
+        {
+            CreateMap<DogDto, Dog>()
+                .ForMember(dog => dog.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dog => dog.Color, opt => opt.MapFrom(src => src.Color))
+                .ForMember(dog => dog.TailLength, opt => opt.MapFrom(src => src.TailLength))
+                .ForMember(dog => dog.Weight, opt => opt.MapFrom(src => src.Weight));   
+        }
+    }
+}
---
+++ DogsAPI.Backend/src/Core/DogsAPI.Backend.Core/Dtos/DogDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogsAPI.Backend.Core.Dtos
+{
+    public class DogDto
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public float TailLength { get; set; }
+        public float Weight { get; set; }
+    }
+}
